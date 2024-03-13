59b68ecb5f177e9f8f40d9d96d2708d40f8d9a16
JesseTNRoberts <34480689+JesseTNRoberts@users.noreply.github.com>
2024-03-13 10:38:26 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import find_packages, setup
+setup(name="minicons",
+      version="0.1",
+      platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+      packages=['minicons'],
+      )
